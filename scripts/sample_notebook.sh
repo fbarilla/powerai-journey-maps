ac1799ee2dac1e6f38f364bf085d17cc774395c9
fbarilla <fbarilla@us.ibm.com>
2017-09-08 14:29:27 -0500

    Update sample_notebook.sh

--- scripts/sample_notebook.sh
+++ scripts/sample_notebook.sh
@@ -4,6 +4,6 @@ if [ ! -d /data/demo ]; then
     cp -a /usr/local/samples /data/demo
 fi
 
-cp /usr/local/samples/motd /etc
+cat /usr/local/samples/motd >> /etc/motd
 sudo /etc/init.d/ssh start
 exec /usr/local/bin/nimbix_notebook "$@"
